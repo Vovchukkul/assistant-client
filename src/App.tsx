69e98a040d1f1bc375bd3467234175e89d122b75
Vovchuk <kulivackijvoloda@gmail.com>
2024-02-15 21:54:32 +0200

    add

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import {
+  HashRouter as Router, Routes, Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
